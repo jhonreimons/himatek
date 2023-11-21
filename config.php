e742a41e2961097039d031b7372b796c8542b6d6
jhonreimons <jhonreimons95@gmail.com>
2023-11-21 11:16:08 +0700

    mengubah koneksi database dari lokal ke RDS

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-     $username ='root';
-     $password ='';
-     $server   ='localhost';
+     $username ='admin';
+     $password ='admin123.,';
+     $server   ='database-jhon.cu18g6uuobva.us-east-1.rds.amazonaws.com';
      $db_name  ='himatek';
 
      $conn = mysqli_connect("$server","$username","$password","$db_name");
